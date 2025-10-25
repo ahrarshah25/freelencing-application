9d5dcfc3164e7b5ca94e9848baf9063cf1029f77
Ahrar Shah <ahrar.09321@gmail.com>
2025-10-25 16:01:52 -0700

    Error Fixed - Third Commit

--- javascript/settings.js
+++ javascript/settings.js
@@ -3,11 +3,7 @@ console.log("Settings JS Connected!");
 var userFound = localStorage.getItem("userFound" , "true");
 var loginType = localStorage.getItem("login");
 if(userFound === "true"){
-    if(loginType === "client"){
-        window.location.href = "/dashboard/client/dashboard.html"
-    }else if(loginType === "developer"){
-        window.location.href = "/dashboard/developer/dashboard.html"
-    }
+    
 }else{
     alert("Please Login First!")
     window.location.href = "/login/login.html"
