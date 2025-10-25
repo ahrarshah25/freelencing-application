1cfc532d56aeed8491da0e2b4c65c88a8952cbeb
Ahrar Shah <ahrar.09321@gmail.com>
2025-10-25 15:59:38 -0700

    Error Fixed

--- javascript/proposals.js
+++ javascript/proposals.js
@@ -1,11 +1,7 @@
 var userFound = localStorage.getItem("userFound" , "true");
 var loginType = localStorage.getItem("login");
 if(userFound === "true"){
-    if(loginType === "client"){
-        window.location.href = "/dashboard/client/dashboard.html"
-    }else if(loginType === "developer"){
-        window.location.href = "/dashboard/developer/dashboard.html"
-    }
+    
 }else{
     alert("Please Login First!")
     window.location.href = "/login/login.html"
